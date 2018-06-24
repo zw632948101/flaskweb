4bf5847b891c189cf66375c43e62f9dc15bffa43
63294 <632948101@qq.com>
2018-06-24 21:32:36 +0800

    是的

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# ! _*_ coding:utf-8 _*_
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import config
+
+__author__ = 'wei.zhang'
+
+"""创建构造文件,导入正在使用的flask扩展"""
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    # 附加路由和自定义的错误页面
+    return app
