58e6d22416ca8f1d0d917f5db33df9fadf46d1a8
63294 <632948101@qq.com>
2018-06-24 21:35:22 +0800

    是的

--- app/eamil.py
+++ app/eamil.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # ! _*_ coding:utf-8 _*_
 __author__ = 'wei.zhang'
-from flask import render_template
+from flask import render_template,current_app
 from . import mail, create_app
 from threading import Thread
 from flask_mail import Message
 
 
 def send_email(to, subject, templte, **kwargs):
-    app = create_app().__getattribute__()
+    app = current_app
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['FLASKY_MAIL_SENDER'],
                   recipients=[to])
     msg.body = render_template(templte + '.txt', **kwargs)
