94f6daff3d949db2231747966b08510ebaab3c86
63294 <632948101@qq.com>
2018-07-01 17:21:51 +0800

    解决冲突

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 # ! _*_ coding:utf-8 _*_
-<<<<<<< HEAD
-
 from flask import Blueprint
 
 main = Blueprint('main', __name__)
-=======
-from flask import Blueprint
-
-main = Blueprint('main', __name__)
-
->>>>>>> exercise_email_porject_20180623_zhangwei
 from . import views, errors
 
 __author__ = 'wei.zhang'
